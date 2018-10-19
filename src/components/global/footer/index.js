11e85446ef8ef249474aaf3f159873aed1cac6f4
WladisLove <klim12121997@yandex.by>
2018-10-19 17:43:55 +0300

    add links to icons in footer

--- src/components/global/footer/index.js
+++ src/components/global/footer/index.js
@@ -3,7 +3,39 @@ import { Link } from 'preact-router';
 import style from './style.scss';
 import { routes } from '../../app'
 
+const socialNetworksArr = ['facebook','linkedin','twitter','instagram'/*,'medium'*/,'youtube'];
+const socialNetworks = {
+	facebook: {
+		icon: '/assets/icons/f-19x19.png',
+		link: 'https://www.facebook.com/telmie.pro/',
+	},
+	linkedin: {
+		icon: '/assets/icons/in-19x19.png',
+		link: 'https://www.linkedin.com/company/telmie',
+	},
+	twitter: {
+		icon: '/assets/icons/tw-38×32.png',
+		link: 'https://twitter.com/TelmieUK',
+	},
+	instagram: {
+		icon: '/assets/icons/inst-20x20.png',
+		link: 'https://www.instagram.com/telmieuk/',
+	},
+	/*medium: {
+		icon: '/assets/icons/m-40×28.png',
+		link: '',
+	},*/
+	youtube: {
+		icon: '/assets/icons/yt-46×34.png',
+		link: 'https://www.youtube.com/channel/UCwphxwpZoBIT6Eg7_FFVzQQ',
+	},
+}
+
 export default class Footer extends Component {
+	onIconClick = (e) => {
+		const {alt} = e.target;
+		alt && window.open(socialNetworks[alt].link);
+	}
 	render() {
 		return (
 			<footer id={style.footer} className='uk-navbar uk-navbar-container'>
@@ -28,12 +60,9 @@ export default class Footer extends Component {
 						</ul>
 					</nav>
 					<div class={style.socialIcons}>
-						<img src='/assets/icons/f-19x19.png' alt='facebook'/>
-						<img src='/assets/icons/in-19x19.png' alt='linkedin'/>
-						<img src='/assets/icons/tw-38×32.png' alt='twitter'/>
-						<img src='/assets/icons/inst-20x20.png' alt='instagram'/>
-						<img src='/assets/icons/m-40×28.png' alt='medium'/>
-						<img src='/assets/icons/yt-46×34.png' alt='youtube'/>
+						{socialNetworksArr.map(el => (
+							<img src={socialNetworks[el].icon} key={el} onClick={this.onIconClick} alt={el}/>
+						))}
 					</div>
 				</div>
 			</footer>
