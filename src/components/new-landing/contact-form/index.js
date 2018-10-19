e031ab38ce17e653a121f2a6848c25530942947b
WladisLove <klim12121997@yandex.by>
2018-10-19 10:03:23 +0300

    add functional and validation to contact form

--- src/components/new-landing/contact-form/index.js
+++ src/components/new-landing/contact-form/index.js
@@ -1,23 +1,65 @@
-import { h } from 'preact';
+import { h, Component } from 'preact';
+import SimpleReactValidator from 'simple-react-validator';
+
 import style from './style.scss';
 
-const ContactForm = () => {
+export default class ContactForm extends Component {
+    constructor(props){
+        super(props);
 
-    return (
-        <div class={style.contuctContainer}>
-            <div class={style.header}>Contact us</div>
-            <div class={style.subHeader}>Any questions? Drop us a line.</div>
-            
-            <div class={style.contactForm}>
-                <input class='new-input' placeholder='Your name'/>
-                <input class='new-input' placeholder='Your email'/>
-                <input class='new-input' placeholder='Company'/>
-                <input class='new-input' placeholder='Subject'/>
-                <input class='new-input' placeholder='Your message'/>
-                <button class='red-btn'>Submit</button>
+        this.state = {
+            name: '',
+            email: '',
+            company: '',
+            subject: '',
+            message: '',
+        }
+        this.validator = new SimpleReactValidator();
+    }
+
+    sendHandler = () => {
+        this.validator.allValid() ? (
+            console.log("OK", this.state)
+        ) : (
+            this.validator.showMessages(),
+			this.forceUpdate()
+        );
+    }
+
+    onChangeHandler = (e) => {
+        const {name, value} = e.target;
+        console.log(name, value)
+        this.setState({ [name]: value });
+    }
+
+    render(){
+        const {name,email,company,subject,message} = this.state;
+        return (
+            <div class={style.contuctContainer}>
+                <div class={style.header}>Contact us</div>
+                <div class={style.subHeader}>Any questions? Drop us a line.</div>
+                
+                <div class={style.contactForm}>
+                    <div className="input-container">
+                        <input type="text" class='new-input' value={name} name="name" placeholder='Your name' onChange={this.onChangeHandler} />
+                        {this.validator.message('name', name, 'required', 'validation-tooltip', {required: 'Please enter name'})}
+                    </div>
+                    <div className="input-container">
+                        <input class='new-input' value={email} name="email" placeholder='Your email' onChange={this.onChangeHandler} />
+                        {this.validator.message('email', email, 'required|email', 'validation-tooltip', {required: 'Please enter email', email: 'Please enter correct email'})}
+                    </div>
+                    <input class='new-input' value={company} name="company" placeholder='Company' onChange={this.onChangeHandler} />
+                    <input class='new-input' value={subject} name="subject" placeholder='Subject' onChange={this.onChangeHandler} />
+                    <div className="input-container">
+                        <input class='new-input' value={message} name="message" placeholder='Your message' onChange={this.onChangeHandler} />
+                        {this.validator.message('message', message, 'required', 'validation-tooltip', {required: 'Please enter message'})}
+                    </div>
+
+                    <button class='red-btn' onClick={this.sendHandler}>
+                        Submit
+                    </button>
+                </div>
             </div>
-        </div>
-	)
+        )
+    }
 }
-
-export default ContactForm;
