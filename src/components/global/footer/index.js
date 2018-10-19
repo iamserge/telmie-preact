6442783b0194659978f050f3d502e6d97434ee5b
WladisLove <klim12121997@yandex.by>
2018-10-19 09:11:56 +0300

    new footer

--- src/components/global/footer/index.js
+++ src/components/global/footer/index.js
@@ -11,19 +11,30 @@ export default class Footer extends Component {
 					<Link href={routes.HOME} id={style.logo}>
 						<img src="/assets/logo.png" alt="Telmie App" />
 					</Link>
-					<span>Copyright &copy;2017 Grandier Developments Ltd, London, UK</span>
+					<div class={style.copyright}>Copyright &copy;2018 TELMIE UK LTD., London, UK</div>
 				</div>
 				<div className="uk-navbar-right"  id={style.right}>
-					<nav>
-						<ul className="uk-navbar-nav" id={style.footerLinks}>
+					<nav id={style.footerLinks}>
+						<ul className="uk-navbar-nav" >
 							<li><Link href={routes.HOME}>Home</Link></li>
-							<li><Link href={routes.ABOUT_US}>About us</Link></li>
-							<li><Link href={routes.FAQ}>FAQ</Link></li>
+							<li><Link href={routes.ABOUT_US}>Company</Link></li>
+							{/*<li><Link href={''}>Testimonials</Link></li>*/}
+							<li><Link href={routes.CONTACT_US}>Contact</Link></li>
+							{/*<li><Link href={''}>Blog</Link></li>*/}
 							<li><Link href={routes.TERMS}>Terms</Link></li>
-							<li><Link href={routes.PRIVACY}>Privacy</Link></li>
-							<li><Link href={routes.CONTACT_US}>Contacts</Link></li>
+							{/*<li><Link href={''}>Help</Link></li>*/}
+							<li><Link href={routes.PRIVACY}>Privacy Policy</Link></li>
+							<li><Link href={routes.FAQ}>FAQ</Link></li>
 						</ul>
 					</nav>
+					<div class={style.socialIcons}>
+						<img src='/assets/icons/f-19x19.png' alt='facebook'/>
+						<img src='/assets/icons/in-19x19.png' alt='linkedin'/>
+						<img src='/assets/icons/tw-38×32.png' alt='twitter'/>
+						<img src='/assets/icons/inst-20x20.png' alt='instagram'/>
+						<img src='/assets/icons/m-40×28.png' alt='medium'/>
+						<img src='/assets/icons/yt-46×34.png' alt='youtube'/>
+					</div>
 				</div>
 			</footer>
 		);
