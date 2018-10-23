b6067f7968a4130eaadb21cf73a515c049fc8dfb
WladisLove <klim12121997@yandex.by>
2018-10-23 10:31:11 +0300

    round to-top-btn; fix position of info-section; fix positions of cards after disappearance

--- src/components/new-landing/info-component/index.js
+++ src/components/new-landing/info-component/index.js
@@ -6,9 +6,9 @@ const InfoComponent = ({wordsToPrint = [], appLink = ''}) => {
     const downloadApp = () => appLink && window.open(appLink);
 
     return (
-        <div class={`${style.infoContainer} uk-container`}>
+        <div class={`${style.infoContainer} uk-container-big`}>
             <div class={style.title}>
-                Video-calls with <AutoPrintText words={wordsToPrint}/> experts
+                Video calls with <AutoPrintText words={wordsToPrint}/> experts
             </div>
             <div class={style.subTitle}>Telmie is the easiest way to talk to an expert that you can trust.</div>
 
