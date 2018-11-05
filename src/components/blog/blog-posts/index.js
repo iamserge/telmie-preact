f18fdb6dccf534625ae538ab31e22c0c53a913af
Irina Yurkevich <i.yurkevich@softteco.com>
2018-11-05 14:11:50 +0300

    removed unusabled code

--- src/components/blog/blog-posts/index.js
+++ src/components/blog/blog-posts/index.js
@@ -78,44 +78,4 @@ class BlogPosts extends Component {
   }
 }
 
-/*const BlogPosts = ({blogPosts = []}) => {
-
-  return (
-    <div>
-      <div class={style.blogPosts}>
-
-          <CarouselProvider
-            className={style.blogPostsSlider}
-            naturalSlideWidth={294}
-            naturalSlideHeight={372}
-            totalSlides={blogPosts.length}
-            visibleSlides={5}
-          >
-            <div class={`${style.blogPostsTitle} uk-container`}>
-              <h3>Other posts</h3>
-              <div class={style.blogPostsArrows}>
-                <ButtonBack><FontAwesome name="angle-left" size="2x" /></ButtonBack>
-                <ButtonNext><FontAwesome name="angle-right" size="2x" /></ButtonNext>
-              </div>
-            </div>
-            <Slider>
-            {blogPosts.map(post => (
-              <li class={style.blogPost} key={post.id} style={{
-                background: `url('${post.img}') no-repeat center`,
-                backgroundSize: "auto 100%"
-              }}>
-                <div class={style.blogPostDescription}>
-                  <p class={style.date}>{post.date}</p>
-                  {post.title}
-                  <Link href={post.link} class="red-btn">Full story</Link>
-                </div>
-              </li>
-            ))}
-            </Slider>
-          </CarouselProvider>
-      </div>
-    </div>
-  )
-}*/
-
 export default BlogPosts;
