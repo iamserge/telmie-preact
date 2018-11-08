5111b8e2de5122d0c097abbf8ba7bca35da27d64
Irina Yurkevich <i.yurkevich@softteco.com>
2018-11-08 16:37:17 +0300

    changed carousel

--- src/components/blog/blog-posts/index.js
+++ src/components/blog/blog-posts/index.js
@@ -1,9 +1,34 @@
 import { h, Component } from 'preact';
-import style from './style.scss';
 import { Link } from 'preact-router';
 import FontAwesome from 'react-fontawesome';
-import { CarouselProvider, Slider, ButtonBack, ButtonNext } from 'pure-react-carousel';
-import 'pure-react-carousel/dist/react-carousel.es.css';
+/*import { CarouselProvider, Slider, ButtonBack, ButtonNext } from 'pure-react-carousel';
+import 'pure-react-carousel/dist/react-carousel.es.css';*/
+import Slider from "react-slick";
+
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+import style from './style.scss';
+
+function SampleNextArrow(props) {
+  const { className, style, onClick } = props;
+  const rightPosition = (window.screen.width-1200)/2;
+  return (
+    <div className={className} onClick={onClick} style={{ ...style, right: `${rightPosition}px` }}>
+      <FontAwesome name="angle-right" size="2x" />
+    </div>
+  );
+}
+
+function SamplePrevArrow(props) {
+  const { className, onClick } = props;
+  const rightPosition = (window.screen.width-1200)/2+64;
+  return (
+    <div className={className} onClick={onClick} style={{ ...style, right: `${rightPosition}px` }}>
+      <FontAwesome name="angle-left" size="2x" />
+    </div>
+  );
+}
+
 
 class BlogPosts extends Component {
 
@@ -34,14 +59,69 @@ class BlogPosts extends Component {
     window.addEventListener('resize', this.updateNumberOfSlides());
   }
 
-
   render(){
     const allPosts = this.props.blogPosts;
+    const settings = {
+      infinite: true,
+      centerMode: true,
+      adaptiveHeight: true,
+      slidesToShow: 4,
+      slidesToScroll: 1,
+      variableWidth: true,
+     // initialSlide: 1,,
+      nextArrow: <SampleNextArrow />,
+      prevArrow: <SamplePrevArrow />,
+      responsive: [
+        {
+          breakpoint: 1199,
+          settings: {
+            slidesToShow: 3
+          }
+        },
+        {
+          breakpoint: 979,
+          settings: {
+            slidesToShow: 2,
+            swipeToSlide: true
+          }
+        },
+        {
+          breakpoint: 639,
+          settings: {
+            slidesToShow: 1
+          }
+        }
+      ]
+    };
 
     return (
       <div>
         <div class={style.blogPosts}>
 
+          <div class={`${style.blogPostsTitle} uk-container`}>
+            <h3>Other posts</h3>
+          </div>
+
+          <Slider {...settings} className={style.blogPostsSlider}>
+            {allPosts.map(post => (
+              <div>
+                <div class={style.blogPost} key={post.id} style={{
+                  background: `url('${post.img}') no-repeat center`,
+                  backgroundSize: "auto 100%"
+                }}>
+                  <Link href={post.link}>
+                    <div class={style.blogPostDescription}>
+                      <p class={style.date}>{post.date}</p>
+                      <p class={style.title}>{post.title}</p>
+                      <button class="red-btn">Full story</button>
+                    </div>
+                  </Link>
+                </div>
+              </div>
+            ))}
+          </Slider>
+
+{/*
           <CarouselProvider
             className={style.blogPostsSlider}
             naturalSlideWidth={294}
@@ -74,6 +154,7 @@ class BlogPosts extends Component {
               ))}
             </Slider>
           </CarouselProvider>
+*/}
         </div>
       </div>
     )
