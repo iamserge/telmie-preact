72590dacba4158e0228bf1c0cc7097b2f27bd4c5
Irina Yurkevich <i.yurkevich@softteco.com>
2018-11-16 11:35:04 +0300

    corrected buttons for featured services block

--- src/components/new-landing/service-card/index.js
+++ src/components/new-landing/service-card/index.js
@@ -6,15 +6,16 @@ const ServiceCard = ({background, serviceName, description, link}) => {
     const cardStyle = {background: `url('${background}') no-repeat center`, backgroundSize: "cover"};
     
     return (
-        <div style={cardStyle} class={style.serviceCard}>
-          <Link href={link} class={style.serviceDescription}>
-            <div class={style.serviceName}>{serviceName}</div>
-            <div class={style.serviceInfo}>
-               {description}
+
+          <Link href={link} style={cardStyle} class={style.serviceCard}>
+            <div class={style.serviceDescription}>
+              <div class={style.serviceName}>{serviceName}</div>
+              <div class={style.serviceInfo}>
+                 {description}
+              </div>
+              <button class='white-btn'>Start learning</button>
             </div>
-            <button class='white-btn'>Start learning</button>
           </Link>
-        </div>
 	)
 }
 
