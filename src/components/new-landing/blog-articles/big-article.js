03d7fdbe4add11c5a6c01d4fd1b21ef301009d34
WladisLove <klim12121997@yandex.by>
2018-11-19 09:41:37 +0300

    blog article hover styles

--- src/components/new-landing/blog-articles/big-article.js
+++ src/components/new-landing/blog-articles/big-article.js
@@ -1,18 +1,18 @@
 import { h } from 'preact';
 import style from './style.scss';
-import { Link } from 'preact-router';
+import { route } from 'preact-router';
 
 const BigArticle = ({title, date,link, img}) => {    
     const articleStyle =  {background: `url('${img}') no-repeat center`, backgroundSize: "auto 100%"}
-
+    const onClick = () => route(link);
     return (
-        <Link href={link} class={style.bigArticle} style={articleStyle}>
+        <div href={link} class={style.bigArticle} style={articleStyle}>
             <div class={style.articleInfo}>
                 <div class={style.date}>{new Date(date).customParse()}</div>
                 <div class={style.title}>{title}</div>
-                <button class='red-btn' >Full story</button>
+                <button class='red-btn' onClick={onClick}>Full story</button>
             </div>
-        </Link>
+        </div>
 	)
 }
 
