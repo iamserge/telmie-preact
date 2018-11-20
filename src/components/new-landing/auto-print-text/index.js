d8ccc64686018a7079eee29ba944cd64f431d74c
WladisLove <klim12121997@yandex.by>
2018-11-20 14:28:34 +0300

    change typing text

--- src/components/new-landing/auto-print-text/index.js
+++ src/components/new-landing/auto-print-text/index.js
@@ -1,12 +1,53 @@
 import { h, Component } from 'preact';
-import { init } from 'ityped'
 import style from './style.scss';
 
 const _words = ["Sports Coach", "Immigration Adviser", "Dietary Guru", "Fashion Expert", "Growth Hacker", "Career Coach", "Wedding Planner", "GDPR Expert", "Business Adviser", "Language Speaker", "Startup Ninja", "Design Consultant", "Scale-up Hacker", "SEO Rock Star", "Pets Guru", "Marketing Ninja"];
 class AutoPrintText extends Component {
+    constructor(props){
+        super(props);
+
+        this.state = {
+            txt: '',
+        }
+    }
+
+    txtType = (period) => {
+        this.loopNum = 0;
+        this.period = parseInt(period, 10) || 2000;
+        this.txt = '';
+        this.tick();
+        this.isDeleting = false;
+    };
+
+    tick = () => {
+        let i = this.loopNum % _words.length,
+            fullTxt = _words[i];
+    
+        this.isDeleting ? 
+            this.setState(prev => ({txt : fullTxt.substring(0, prev.txt.length - 1)}))
+            : this.setState(prev => ({txt : fullTxt.substring(0, prev.txt.length + 1)}));
+        
+        let delta = 200 - Math.random() * 100;    
+        this.isDeleting && (delta /= 2);
+    
+        if (!this.isDeleting && this.state.txt === fullTxt) {
+            delta = this.period;
+            this.isDeleting = true;
+        } else if (this.isDeleting && this.state.txt === '') {
+            this.isDeleting = false;
+            this.loopNum++;
+            delta = 100;
+        }
+    
+        this.timeout = setTimeout(() => this.tick(), delta);
+    };
     
     componentDidMount(){
-        init(this.wordArea, { showCursor: false, typeSpeed:  150, backDelay:  1000, strings: _words });
+        this.txtType(1500);
+    }
+
+    componentWillUnmount(){
+        clearTimeout(this.timeout);
     }
 
     componentDidUpdate(_, prevState){
@@ -17,7 +58,7 @@ class AutoPrintText extends Component {
 
         return (
             <div class={style.wordContainer}>
-                <span class={style.word} ref={ref => this.wordArea = ref} />
+                <span class={style.word}>{this.state.txt}</span>
             </div>
         )
     }
