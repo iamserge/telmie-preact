68465f939e8f9e02eba6f4e5a86f49c8dcbe67de
WladisLove <klim12121997@yandex.by>
2018-12-03 20:06:57 +0300

    add inview animation for minutes

--- src/components/new-landing/photo-cards/photo-card/index.js
+++ src/components/new-landing/photo-cards/photo-card/index.js
@@ -1,19 +1,20 @@
-import { h, Component } from 'preact';
+import { h } from 'preact';
 import Hr from '../../../hr'
+import TrackVisibility from 'react-on-screen';
+
 import style from './style.scss';
 
-const PhotoCard = props => {
-    const {isStat, minutes = 0, name, img, serviceName, price, time, cardStyle = {}} = props;
+const StatCard = (props) => {
+    const {minutes = 0, cardStyle = {}} = props;
     let minutesArr = [];
     for(let i = 0, max = 10; i < max; i++){
         minutesArr.push(Math.floor(minutes*i/max));
     }
     minutesArr.push(minutes);
-
-    return isStat ? (
+    return (
         <div class={`${style.photoCard} ${style.statCard}`} style={cardStyle}>
             <div class={style.minWrapper}>
-                <div class={style.minutes}>
+                <div class={`${style.minutes} ${props.isVisible && style.animateMinutes}`}>
                     {minutesArr.map(el => (<div>{Number(el).toLocaleString()}</div>))}
                 </div>
             </div>
@@ -21,6 +22,16 @@ const PhotoCard = props => {
             <Hr color='white' width={40} margin={20} />
             <span class={style.signature}>Communicated via Telmie</span>
 		</div>
+    )
+}
+
+const PhotoCard = props => {
+    const {isStat, name, img, serviceName, price, time, cardStyle = {}} = props;
+    
+    return isStat ? (
+        <TrackVisibility once>
+            <StatCard {...props}/>
+        </TrackVisibility>
 	) : (
         <div class={style.photoCard} style={cardStyle}>
 			<img src={img}/>
