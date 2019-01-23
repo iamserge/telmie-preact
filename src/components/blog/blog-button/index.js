b722ee98964f514ebc578acdf53469252d374408
WladisLove <klim12121997@yandex.by>
2019-01-23 17:21:08 +0300

    add GA tracking for download btn in Blog

--- src/components/blog/blog-button/index.js
+++ src/components/blog/blog-button/index.js
@@ -1,11 +1,18 @@
 import { h } from 'preact';
 import { processBlogBtn } from "../../../utils/prismic-middleware";
 import { route } from 'preact-router';
+import ReactGA from 'react-ga';
+import { labelsGA } from "../../../utils/consts";
 
 const Button = ({content}) => {
     const { text, link, isExternal } = processBlogBtn(content);
     
-    const onClick = () => link && (isExternal ? window.open(link) : route(link));
+    const onClick = () => link && (isExternal ? 
+        ReactGA.outboundLink({
+            label: labelsGA.downloadAppClick
+        }, () => window.location.assign(link))
+        : route(link));
+
 
     return text && <div style={{textAlign: 'center', padding: 5}}>
             <button class='red-btn' onClick={onClick}>{text}</button>
